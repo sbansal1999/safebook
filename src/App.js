f152e616b03dd777cff7892b995f9a3087b3d0f7
sbansal1999 <sbansal1999@gmail.com>
2022-10-12 23:45:19 +0530

    Installed react-router and created various routes to work on

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Chat from "./components/Chat";
+import Feed from "./components/Feed";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import Post from "./components/Post";
+import Profile from "./components/Profile";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>Satyam Bansal123</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
