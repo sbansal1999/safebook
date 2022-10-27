0dc9196539bbe21fbee52cb5a67cf0c07f63c21d
Nistha <nisthav8@gmail.com>
2022-10-28 01:05:22 +0530

    some changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
 import NavLoggedIn from "./NavLoggedIn";
+import { Table } from "react-bootstrap";
 
 export default function Profile() {
   const [pic, setPic] = useState(
@@ -14,11 +18,32 @@ export default function Profile() {
     "Kamalpreet Kaur",
     "Vipul Singh",
     "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
+    "Kamalpreet Kaur",
+    "Vipul Singh",
+    "Satyam Bansal",
   ]);
   const [show, setShow] = useState(false);
   return (
     <div>
-      <NavLoggedIn/>
+      <NavLoggedIn />
       <Card style={{ width: "20rem" }}>
         <Card.Img variant="top" src={pic} width="400" />
         <Card.Body>
@@ -29,18 +54,64 @@ export default function Profile() {
           <Button
             variant="primary"
             onClick={() => {
-              if (show == true) setShow(false);
+              if (show === true) setShow(false);
               else setShow(true);
             }}
           >
             Show Friends
           </Button>
-          <br />
+          <br/>
           <Button className="mt-3" variant="success">
             Show Media
           </Button>
         </Card.Body>
       </Card>
+
+      <Modal show={show} aria-labelledby="contained-modal-title-vcenter">
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">Friends</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+          <Table responsive striped bordered hover className="mt-3">
+            <tbody>
+              {friends.map((friend) => {
+                return (
+                  <tr>
+                    <td>profile pic</td>
+
+                    <td>{friend}</td>
+
+                    <td>
+                      <Button variant="primary" onClick={() => {}}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="25"
+                          height="25"
+                          fill="currentColor"
+                          class="bi bi-chat-right"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z" />
+                        </svg>
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <tbody></tbody>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
