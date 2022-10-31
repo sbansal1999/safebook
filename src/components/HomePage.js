4a7e6acdf476ec1cccce9f3ac635902743b4d9d6
sbansal1999 <sbansal1999@gmail.com>
2022-10-31 18:17:53 +0530

    Modified HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,40 @@
 import React from "react";
 import NavBar from "./NavBar";
-import background_image from "../images/background.jpg"
+import background_image from "../images/background.jpg";
 export default function HomePage() {
   return (
     <div>
       <NavBar />
-      <br />
-      <h6 style={{ fontSize: "50px" }}> SAFE</h6>
-      <img src={background_image} style={{ height: "350px", width: "auto" }} />
-      <h6 style={{ fontSize: "50px" }}>BOOK</h6>
+      <div
+        className="d-flex justify-content-center align-items-center mt-5"
+        style={{ width: "100%" }}
+      >
+        <div
+          className="d-flex flex-column align-items-end"
+          style={{ width: "15vw" }}
+        >
+          <h1 className="huge">S</h1>
+          <h1 className="huge">A</h1>
+          <h1 className="huge">F</h1>
+          <h1 className="huge">E</h1>
+        </div>
+        <div
+          className="d-flex justify-content-center"
+          style={{ width: "70vw" }}
+        >
+          <br />
+          <img src={background_image} style={{ width: "80%" }} />
+        </div>
+        <div
+          className="d-flex flex-column align-items-start"
+          style={{ width: "15vw" }}
+        >
+          <h1 className="huge">B</h1>
+          <h1 className="huge">O</h1>
+          <h1 className="huge">O</h1>
+          <h1 className="huge">K</h1>
+        </div>
+      </div>
     </div>
   );
 }
-
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import NavBar from "./NavBar";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export default function Register() {
   const [fName, setFName] = useState();
@@ -12,78 +15,87 @@ export default function Register() {
   const [confirmPassword, setConfirmPassword] = useState();
   return (
     <div>
-      Register
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>First Name </Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter First Name"
-            value={fName}
-            onChange={(e) => {
-              setFName(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Last Name </Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Last Name"
-            value={lName}
-            onChange={(e) => {
-              setLName(e.target.value);
-            }}
-          />
-        </Form.Group>
+      <NavBar />
+      <div
+        className="d-flex justify-content-center mt-5"
+        style={{ width: "100%" }}
+      >
+        <Form className="border p-5 rounded">
+          <h3>Register</h3>
+          <Form.Group className="mb-3" as={Row}>
+            <Form.Label column sm={2}>
+              First Name
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="text"
+                placeholder="Enter First Name"
+                value={fName}
+                onChange={(e) => {
+                  setFName(e.target.value);
+                }}
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Last Name </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Last Name"
+              value={lName}
+              onChange={(e) => {
+                setLName(e.target.value);
+              }}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter E-Mail Address"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Date of Birth</Form.Label>
-          <Form.Control
-            type="date"
-            placeholder="Enter DOB"
-            value={DOB}
-            onChange={(e) => {
-              setDOB(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => {
-              setConfirmPassword(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Button type="submit">Register</Button>
-      </Form>{" "}
-      {fName}
+          <Form.Group className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter E-Mail Address"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Date of Birth</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Enter DOB"
+              value={DOB}
+              onChange={(e) => {
+                setDOB(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Button type="submit">Register</Button>
+        </Form>
+      </div>
     </div>
   );
 }
