7c960ccf105946407602e90f566a678ebe12b92e
sbansal1999 <sbansal1999@gmail.com>
2022-11-09 02:42:53 +0530

    added modal to friends in sidenavbar

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,7 +1,8 @@
-import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Image } from "react-bootstrap";
+import { Button, Card, Image, Table } from "react-bootstrap";
 import { useCookies } from "react-cookie";
+
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 import { db } from "./firebase-config";
 
@@ -9,11 +10,26 @@ import { Link, useNavigate } from "react-router-dom";
 
 import profile_image from "./../images/profile.jpg";
 
+import Modal from "react-bootstrap/Modal";
+
+import { BsChatRightFill } from "react-icons/bs";
+
 function SideNavBar() {
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
+  const [friends, setFriends] = useState([]);
+  const [show, setShow] = useState(false);
 
   const [cookies, setCookie, removeCookie] = useCookies(["userId"]);
+
+  const getFriends = async () => {
+    const dbRef = collection(db, "users", cookies.userId, "friends");
+    const newData = await getDocs(dbRef);
+
+    newData.docs.map((doc) => {
+      setFriends((prev) => [...prev, doc.data()]);
+    });
+  };
 
   useEffect(() => {
     const fetchName = async () => {
@@ -26,6 +42,7 @@ function SideNavBar() {
     };
 
     fetchName();
+    getFriends();
   }, []);
 
   return (
@@ -43,13 +60,15 @@ function SideNavBar() {
         </Card>
 
         <div className="d-grid m-3">
-          <Button size="lg" className="mb-3" variant="primary">
-            <Link
-              to="/friends"
-              className="no-css d-flex full-width justify-content-center"
-            >
-              Friends
-            </Link>
+          <Button
+            size="lg"
+            className="mb-3"
+            variant="primary"
+            onClick={() => {
+              setShow(true);
+            }}
+          >
+            Friends
           </Button>
           <Button size="lg" className="mb-3" variant="primary">
             <Link
@@ -77,6 +96,63 @@ function SideNavBar() {
           </Button>
         </div>
       </div>
+
+      <Modal
+        centered
+        show={show}
+        aria-labelledby="contained-modal-title-vcenter"
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">Friends</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+          <Table responsive striped bordered hover className="mt-3">
+            <tbody>
+              {friends.map((friend, id) => {
+                return (
+                  <tr>
+                    <td>
+                      <div
+                        className="d-flex align-items-center"
+                        style={{ height: "5vh" }}
+                      >
+                        <div style={{ width: "20%" }}>
+                          <Image
+                            rounded
+                            src={profile_image}
+                            style={{ width: "50%" }}
+                          />
+                        </div>
+                        <div style={{ width: "60%" }}>
+                          {friend.fname + " " + friend.lname}
+                        </div>
+                        <div
+                          className="d-flex justify-content-center"
+                          style={{ width: "20%" }}
+                        >
+                          <Button variant="primary" onClick={() => {}}>
+                            <BsChatRightFill />
+                          </Button>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <tbody></tbody>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
