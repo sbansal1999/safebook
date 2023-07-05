5665adb9802cecd40ebc5027aafc0756999e0c84
Satyam Bansal <sbansal1999@gmail.com>
2023-07-05 16:21:01 +0530

    Upgrade JS dependencies

--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsBT4FOJ4cZ1ZUf5sZY4heekMnHSVrjH4",
-  authDomain: "safebook-8aa47.firebaseapp.com",
-  projectId: "safebook-8aa47",
-  storageBucket: "safebook-8aa47.appspot.com",
-  messagingSenderId: "336424387603",
-  appId: "1:336424387603:web:61974f4c557270aab06be6",
-  measurementId: "G-DSRQ4N1D7N",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
